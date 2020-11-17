2bf2e5bcde2d9bbaed14d2a71aea271320891345
Nicole Mastrostefano <nicole_mas@hotmail.it>
2020-11-17 17:13:45 +0100

    css

--- js/script.js
+++ js/script.js
@@ -1,37 +1,14 @@
-
-
-//  // Scrivi un programma che stampi i numeri da 1 a 100
-
-//     if ((i % 3 == 0) && (i % 5 == 0)) {
-//     console.log("fizz buzz");
-//     }
-//     else if ( i % 3 == 0 ) {
-//     console.log("Fizz");
-//
-//     } else if( i % 5 == 0 ) {
-//     console.log("Buzz");
-
-
-// prove
-// var numbers = new Array(100);
-//     for (var i = 0; i <= 100; i++) {
-//         numbers[i] = i + 1;
-//         document.write(numbers[i] + '<br>');
-//     }
-//
-
-
 var numbers = document.getElementById("numbers");
   // var num = new Array(100);
-  for (var i = 1; i < 101; i++) {
+  for (var j = 1; j <= 100; j++) {
 
-    if((i % 3 == 0) && (i % 5 == 0)) {
-      j =  "FizzBuzz";
-    } else if (i % 5 == 0) {
-      j =  "Buzz";
-    } else if (i % 3 == 0) {
-      j =  "Fizz";
+    if((j % 3 == 0) && (j % 5 == 0)) {
+      i=  "FizzBuzz";
+    } else if (j % 5 == 0) {
+      i =  "Buzz";
+    } else if (j % 3 == 0) {
+      i =  "Fizz";
     } else {
-      j =  i;
-    }  numbers.innerHTML +=  (j+ '<br>');
+      i =  j;
+    }  numbers.innerHTML +=  (i+ '<br>');
   }
